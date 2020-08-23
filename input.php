144738e187c9c96929abb151f62bc5fdc1a7b7cd
rudigidev <63047104+rudigidev@users.noreply.github.com>
2020-08-23 18:40:24 +0700

    Update input.php

--- input.php
+++ input.php
@@ -14,22 +14,27 @@
 	<a href="index.php">Lihat Semua Data</a>
 
 	<br/>
-	<h3>Input data baru</h3>
+	<h3>Input data produk baru</h3>
 	<form action="input-aksi.php" method="post">		
 		<table>
 			<tr>
-				<td>Nama</td>
-				<td><input type="text" name="nama"></td>					
+				<td>Nama Produk</td>
+				<td><input type="text" name="nama_produk"></td>					
 			</tr>	
 			<tr>
-				<td>Alamat</td>
-				<td><input type="text" name="alamat"></td>					
+				<td>Keterangan</td>
+				<td><input type="text" name="keterangan"></td>					
 			</tr>	
 			<tr>
-				<td>Pekerjaan</td>
-				<td><input type="text" name="pekerjaan"></td>					
-			</tr>	
-			<tr>
+				<td>harga</td>
+				<td><input type="number" name="harga"></td>					
+			</tr>
+                        <tr>
+                                <td>jumlah</td>
+                                <td><input type="number" name="jumlah"</td>	
+                        </tr>
+
+       			<tr>
 				<td></td>
 				<td><input type="submit" value="Simpan"></td>					
 			</tr>				
